f3cc3c5f922677a017a2ed4d3bfff906ad9dc367
Alastairrd <alastairros-davison@hotmail.com>
2021-11-14 05:16:27 +0000

    refactoring of actions
    added baseaction class and refactored slightly

---
+++ Engine/Actions/BaseAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Actions
+{
+    public abstract class BaseAction
+    {
+        protected readonly GameItem _itemInUse;
+
+        public event EventHandler<string> OnActionPerformed;
+
+        protected BaseAction(GameItem itemInUse)
+        {
+            _itemInUse = itemInUse;
+        }
+
+        //protected means it can be called by anything within class or any child classes
+        protected void ReportResult(string result)
+        {
+            OnActionPerformed?.Invoke(this, result);
+        }
+    }
+}
