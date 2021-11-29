02bd522612f64fdcff958d45020dd7b60fd710b5
Alastairrd <alastairros-davison@hotmail.com>
2021-11-29 18:25:32 +0000

    added extension methods

---
+++ Engine/Shared/ExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace Engine.Shared
+{
+
+    //building extension method has 3 rules
+    //class must be static
+    //function must be static
+    //first parameter of extension method you prefix with 'this'
+    public static class ExtensionMethods
+    {
+        public static int AttributeAsInt(this XmlNode node, string attributeName)
+        {
+            return Convert.ToInt32(node.AttributeAsString(attributeName));
+        }
+
+        public static string AttributeAsString(this XmlNode node, string attributeName)
+        {
+            XmlAttribute attribute = node.Attributes?[attributeName];
+
+            if (attribute == null)
+            {
+                throw new ArgumentException($"The attribute '{attributeName} does not exist");
+            }
+
+            return attribute.Value;
+        }
+
+    }
+}
