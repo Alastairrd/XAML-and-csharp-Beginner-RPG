ffad78a34c324b26ab63e12cc60bc6047b4dd93d
Alastairrd <alastairros-davison@hotmail.com>
2021-12-02 20:26:36 +0000

    rest of data to xml

--- Engine/Factories/WorldFactory.cs
+++ Engine/Factories/WorldFactory.cs
@@ -99,7 +99,7 @@ namespace Engine.Factories
             }
 
             location.TraderHere =
-                TraderFactory.GetTraderByName(traderHere.AttributeAsString("Name"));
+                TraderFactory.GetTraderByID(traderHere.AttributeAsInt("ID"));
         }
     }
 }
